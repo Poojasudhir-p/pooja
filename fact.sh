74bd34bdf2141d69e8e6e63fedcb5fe61b808d68
amalvi007 <amalvijayan1661@gmail.com>
2019-03-28 19:34:53 +0530

    modified

--- fact.sh
+++ fact.sh
@@ -1,9 +1,11 @@
 echo "enter a number"
 read num
+
 fact=1
-while [ $num -ge 1 ]
+
+while [ $num -gt 1 ]
 do
-fact=`expr  $fact\* $num`
-num=’expr $num – 1’
+  fact=$((fact * num))
+  num=$((num - 1))
 done
-echo "factorial of $n is $fact"
+echo $fact
